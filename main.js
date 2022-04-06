c4c7503d849cf79f0b4e120b611b4c5a3b629dca
Adam Klekiel <68907576+adam-kl47@users.noreply.github.com>
2022-04-06 13:13:27 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+
+const btn = document.querySelector('button');
+let number = Math.floor(Math.random() * 28) + 1;
+
+const los = () => {
+
+    const txt = document.querySelector('p');
+    
+    if (number == 10) {
+        txt.textContent = 5;
+    }
+    else {
+
+        txt.textContent = number;
+    }
+
+    number = Math.floor(Math.random() * 28) + 1;
+
+}
+
+btn.addEventListener('click', los);
